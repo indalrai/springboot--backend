69fb4c58e2c80abb1cba9e821d1316e245bafe8b
Indal <raiindal619@gmail.com>
2022-10-11 12:02:28 +0530

    first commit

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package net.javaguides.springboot.config;
+
+ 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@SuppressWarnings("deprecation")
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+         http.csrf().disable().cors().disable().authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated()
+         .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+      
+    }
+}
---
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -0,0 +1,64 @@
+package net.javaguides.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+	public EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService employeeService) {
+		super();
+		this.employeeService = employeeService;
+	}
+
+	// build create rest api
+	@PostMapping("/save")
+	public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
+
+		return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+
+	}
+
+	// build get all employee rest api
+	@GetMapping
+	public List<Employee> getAllEmployees() {
+		return employeeService.getAllEmployees();
+	}
+
+	// build api b id
+	@GetMapping("{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long id) {
+		return new ResponseEntity<Employee>(employeeService.getEmployeeById(id), HttpStatus.OK);
+	}
+
+	// update api
+	@PutMapping("{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long id, @RequestBody Employee employee) {
+		return new ResponseEntity<Employee>(employeeService.updateEmployee(employee, id), HttpStatus.OK);
+	}
+
+	// delete
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deleteEmployee(@PathVariable("id") long id) {
+		employeeService.deleteEmployee(id);
+
+		return new ResponseEntity<String>("Employee deleted Successully !", HttpStatus.OK);
+	}
+}
